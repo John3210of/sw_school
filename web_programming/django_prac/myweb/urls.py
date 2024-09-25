295e64fa28710b2220e8e822a60429e4105436ae
John3210of <john3210of@gmail.com>
2024-09-25 16:48:43 +0900

    django project migrate

---
+++ web_programming/django_prac/myweb/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from .views import *
+# from rest_framework.routers import DefaultRouter
+
+# router = DefaultRouter()
+# router.register('item_list',ItemViewSet)
+
+urlpatterns = [
+    # path('', include(router.urls)),
+    path('', IndexView.as_view(), name='index'),
+    path('items/', ItemView.as_view(), name='item'),
+    path('set-cookie/', SetCookieView.as_view(), name='set-cookie'),
+    path('get-cookie/', GetCookieView.as_view(), name='get-cookie'),
+    path('<str:string>/', IndexView.as_view(), name='index-string'),
+]
