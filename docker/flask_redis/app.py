aedc65dfd7237e2cded53f9a556929d04146a216
John3210of <john3210of@gmail.com>
2024-10-18 11:06:08 +0900

    create dockerfile using flask-redis

---
+++ docker/flask_redis/app.py
@@ -0,0 +1,20 @@
+import time
+import redis
+from flask import Flask
+
+app=Flask(__name__)
+db_cache = redis.Redis(host='redis',port=6379)
+# db_cache = redis.Redis()
+
+def web_hit_count():
+    return db_cache.incr('hits')
+
+@app.route('/')
+def home():
+    cnt = web_hit_count()
+    return '''
+            <h1> Docker compose app </h1>
+            <p> Web access count : {} times </p>'''.format(cnt)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9000, debug=True)
